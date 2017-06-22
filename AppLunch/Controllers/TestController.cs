61a9a907846b3e64645302716a2af17e67c3630f
Eric Hansel <ehansel01@gmail.com>
2017-06-22 19:30:47 -0400

    initial commit

---
+++ AppLunch/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AppLunch.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public async Task<IHttpActionResult> Get()
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            var result = await userManager.CreateAsync(new IdentityUser("testing@tester.com"),"Password01");
+            return Ok();
+        }
+    }
+}
