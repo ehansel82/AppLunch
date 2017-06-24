590367bc1b28c67d5db611855e3483f426f634d7
Eric Hansel <ehansel01@gmail.com>
2017-06-23 21:23:02 -0400

    Add logic to create a claim. Start an account controller.

--- AppLunch/Controllers/TestController.cs
+++ AppLunch/Controllers/TestController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -15,10 +16,16 @@ namespace AppLunch.Controllers
         [HttpGet]
         public async Task<IHttpActionResult> Get()
         {
+            var userName = "testing2@tester.com";
+            var userPassword = "Password01";
+
             var userStore = new UserStore<IdentityUser>();
             var userManager = new UserManager<IdentityUser>(userStore);
 
-            var result = await userManager.CreateAsync(new IdentityUser("testing@tester.com"),"Password01");
+            var result = await userManager.CreateAsync(new IdentityUser(userName), userPassword);
+            var user = userManager.FindByName(userName);
+            var claimResult = userManager.AddClaim(user.Id, new Claim("Level", "Admin"));
+
             return Ok();
         }
     }
