da70f252f41e1f958b81790a506b6fc28a09460a
Eric Hansel <ehansel01@gmail.com>
2017-07-08 20:51:56 -0400

    Add ninject.

---
+++ AppLunch/DataAccess/AppRepository.cs
@@ -0,0 +1,16 @@
+﻿using AppLunch.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppLunch.DataAccess
+{
+    public class AppRepository : IAppRepository
+    {
+        public List<Member> GetMembers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppLunch/Interfaces/IAppRepository.cs
@@ -0,0 +1,14 @@
+﻿using AppLunch.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLunch.Interfaces
+{
+    public interface IAppRepository
+    {
+        List<Member> GetMembers();
+    }
+}
